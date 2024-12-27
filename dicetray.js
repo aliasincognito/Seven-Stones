ef9a666c8d0249173e53b22005612a67f052af08
agamemnon-1998 <136354428+agamemnon-1998@users.noreply.github.com>
2024-12-27 01:10:08 -0600

    FIRST WORKING VERSION LETS GOOOOOO

--- dicetray.js
+++ dicetray.js
@@ -1,18 +1,35 @@
 //a class for die with 6 sides
 class Die {
-    constructor({twoHanded = false} = {}) {
+    constructor({twoHanded = false, rerollones = false} = {}) {
+
+        this.currentoutcome = [1, 2, 3, 4, 5, 6];
         this.sides = 6;
-        if (twoHanded) {
-            this.duelOutcomes = [1, 1, 2, 3, 4, 6]; //-1 on duel roll
-        } else {
-            this.duelOutcomes = [1, 2, 3, 4, 5, 6];
-        }
-        if (twoHanded) {
-            this.woundOutcomes = [2, 3, 4, 5, 6, 6]; //+1 on wounding roll // No, we're gonna rework this to just add 1 to the roll capped at 6
-        } else {
-            this.woundOutcomes = [1, 2, 3, 4, 5, 6];
-        }
+        this.max = 6;
+        this.min = 1;
+        this.baseoutcome = [1, 2, 3, 4, 5, 6]; //you may eventually want to modify this to be changed in place for saving the dice for wounding
+        this.twoHanded = twoHanded;
+        this.rerollones = rerollones;
+
     }
+       Result() {
+            let returnArray = this.baseoutcome;
+            //Natural Result Rerolls
+            if (this.rerollones) {
+                returnArray = [1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]; 
+                }
+            
+            //Plus-Minus Modifiers 
+            if (this.twoHanded) {
+                returnArray.forEach((face, index, die) => {
+                    die[index] = (face==1 || face== 6) ? face : face-1;
+                });
+            
+            }
+
+            this.currentoutcome = returnArray;
+            return returnArray;
+
+       }
 }
 
 //a class for a set of dice
@@ -88,43 +105,55 @@ class thisColorIs {
 
 //Adding buttons dynamically
 
-document.addEventListener('DOMContentLoaded', () => {
-    const orderContainer = document.getElementById('orderContainer');
-    const addItemBtn = document.getElementById('addItemBtn');
+const parentElement = document.getElementById('myTab');
+const tabCount = parentElement.querySelectorAll('.nav-item');
+const tabElements = document.querySelectorAll('.tab-pane');
+const orderContainer = document.querySelectorAll('[id^="orderContainer"]');
+const addItemBtn = document.querySelectorAll('[id^="addItemBtn"]');
 
-    addItemBtn.addEventListener('click', () => {
-        const itemId = `item-${Date.now()}`;
-        const itemHtml = `
-            <div class="card mt-3" id="${itemId}">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <input type="text" class="form-control" placeholder="Item Name">
-                        <div class="input-group ml-3">
-                            <div class="input-group-prepend">
-                                <button class="btn btn-secondary" type="button" onclick="changeItemCount('counter-${itemId}', -1)">-</button>
+for (let i=0; i<tabCount.length; i++) {
+    document.addEventListener('DOMContentLoaded', () => {
+
+        addItemBtn[i].addEventListener('click', () => {
+            const itemId = `diceColor-${Date.now()}`;
+            const itemHtml = `
+                <div class="card mt-3" id="${itemId}">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            
+                            <div class="input-group ml-3">
+                                <div class="input-group-prepend">
+                                    <button class="btn btn-secondary" type="button" onclick="changeItemCount('counter-${itemId}', -1)">-</button>
+                                </div>
+                                <input type="text" id="counter-${itemId}" class="form-control text-center" value="0">
+                                <div class="input-group-append">
+                                    <button class="btn btn-secondary" type="button" onclick="changeItemCount('counter-${itemId}', 1)">+</button>
+                                </div>
                             </div>
-                            <input type="text" id="counter-${itemId}" class="form-control text-center" value="0">
-                            <div class="input-group-append">
-                                <button class="btn btn-secondary" type="button" onclick="changeItemCount('counter-${itemId}', 1)">+</button>
-                            </div>
+                            <button class="btn btn-danger ml-3" type="button" onclick="removeItem('${itemId}')">Remove</button>
                         </div>
-                        <button class="btn btn-danger ml-3" type="button" onclick="removeItem('${itemId}')">Remove</button>
                     </div>
-                </div>
 
-                <div class="card-body">
-                    <div class="btn-group" role="group" aria-label="Basic example">
-                        <button type="checkbox" class="btn btn-secondary">Left</button>
-                        <button type="checkbox" class="btn btn-secondary">Middle</button>
+                    <div class="card-body">
+                        <div class="form-check form-check-inline ml-3">
+                            <input class="form-check-input" type="checkbox" id="2h-${itemId}" data-modifier-type="twoHanded">
+                            <label class="form-check-label" for="2h-${itemId}">Two-Handing</label>
+                            
+                        </div>
+                        <div class="form-check form-check-inline ml-3">
+                            <input class="form-check-input" type="checkbox" id="rr1-${itemId}" data-modifier-type="rerollones">
+                            <label class="form-check-label" for="rr1-${itemId}">Rerolling Ones</label>
+                        </div>
                     </div>
+
+
                 </div>
-
-
-            </div>
-        `;
-        orderContainer.insertAdjacentHTML('beforeend', itemHtml);
+            `;
+            orderContainer[i].insertAdjacentHTML('beforeend', itemHtml);
+        });
     });
-});
+
+}
 
 function removeItem(itemId) {
     const itemElement = document.getElementById(itemId);
@@ -133,11 +162,9 @@ function removeItem(itemId) {
 
 function changeItemCount(itemId, change) {
     const itemElement = document.getElementById(itemId);
-    //const inputElement = itemElement.querySelector('input[type="text"]:not([readonly]) + input[readonly]');
     let currentCount = parseInt(itemElement.value, 10);
     currentCount += change;
     if (currentCount < 0) currentCount = 0;
     itemElement.value = currentCount;
-    console.log(currentCount);
-    console.log("changeItemCountCalled");
+
 }
--- undying_lands.js
+++ undying_lands.js
@@ -1,3 +1,34 @@
+//Holds functions for grabbig values and performing calculations
+function createDiceArrays(player) {
+    let diceArray = [];
+    let diceParameters = [];
+    let targetDiv = document.getElementById('orderContainerPlayer');
+    if (player==false) {
+        targetDiv = document.getElementById('orderContainerOpponent');
+    }
+    let diceColors = targetDiv.querySelectorAll('[id^="diceColor-"]');
+    console.log('diceColors', diceColors);
+    for (let i = 0; i < diceColors.length; i++) { //bakes in assumption they're only ever be one inputField for each "dice color"
+        let diceParametersForThisColor = {};
+        let inputField = diceColors[i].querySelectorAll('[id^="counter-"]');
+        let modifiers = diceColors[i].querySelectorAll('.form-check-input:checked'); //Will need a method for handling multiple modifiers
+        let diceCount = inputField[0].value;
+        
+        //Code for building dice parameters set
+        
+        for (let j = 0; j < modifiers.length; j++) {
+            let modifier = modifiers[j].getAttribute('data-modifier-type');
+            diceParametersForThisColor[modifier] = true;
+        }
+        
+        
+        for (let j = 0; j < diceCount; j++) {
+            diceArray.push(new Die(diceParametersForThisColor));
+        }
+    }
+    diceArray.forEach((element, index, array) => array[index] = element.Result())
+    return diceArray;
+}
 
 
 // creates all possible arrays of outcomes - Copilot helped with this
@@ -26,14 +57,10 @@ function generateCombinations(arrays) {
 
 // Counts victories for the player
 function countVictories({playerDiceCount, results, playerHasFight=false, tiedFight=true}) { //need to add elven blade
-    console.log('playerDiceCount:', playerDiceCount);
-    console.log('results:', results);
-    console.log('playerHasFight:', playerHasFight);
-    console.log('tiedFight:', tiedFight);
 
     let victories = 0;
-    let outcomes = results.length;
-    for (let x = 0; x < results.length; x++) {
+    const outcomes = results.length;
+    for (let x = 0; x < outcomes; x++) {
         let outcome = results[x];
         let playerOutcome = Math.max(...(outcome.slice(0, playerDiceCount)));
         let opponentOutcome = Math.max(...(outcome.slice(playerDiceCount)));
@@ -52,7 +79,34 @@ function countVictories({playerDiceCount, results, playerHasFight=false, tiedFig
             }
         }
     }
-    console.log('victories:', victories);
-    console.log('outcomes:', outcomes);
     return victories / outcomes;
 }
+
+function getFightValueState() {
+    let difference = 0;
+    const playerHasFightBaseDiv = document.getElementById('fightValueRadio');
+    const playerHasFight = playerHasFightBaseDiv.querySelectorAll('.form-check-input:checked');
+    for (let i = 0; i < playerHasFight.length; i++) {
+        if (playerHasFight[i].checked)
+            {
+                difference = new Number(playerHasFight[i].getAttribute('data-numeric-value'));
+            };
+    }
+    return difference;
+}
+
+
+function countVictoriesForAllDice() {
+    let playerresults = createDiceArrays(true)
+    let opponentResults = createDiceArrays(false);
+    let allResults = playerresults.concat(opponentResults);
+    let allCombinations = generateCombinations(allResults);
+
+    const fightValueDifference = getFightValueState();
+    const playerHasFight = fightValueDifference > 0 ? true : false;
+    const tiedFight = fightValueDifference == 0 ? true : false;
+
+    let victoryPercentage = countVictories({playerDiceCount: playerresults.length, results: allCombinations, playerHasFight: playerHasFight, tiedFight: tiedFight});
+    document.getElementById('output').innerHTML = (100 * victoryPercentage).toFixed(2) + '%' + ' chance of winning';
+    return ((100 * victoryPercentage).toFixed(2) + '%' + ' chance of winning');
+    }
